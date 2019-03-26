4654e4eb9d1edc4b4af6cc57ece144bf047dcbf7
alissonfelipe1234 <alissonfelipe1234@outlook.com>
2019-03-26 20:19:43 -0300

    create h libary

---
+++ linked_list.h
@@ -0,0 +1,44 @@
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+typedef extern struct _node
+{
+    int value;
+    struct _node *prox;
+} Node;
+
+typedef struct _list
+{
+    Node *first;
+    Node *last;
+    int repeat;
+} List;
+
+List* newList();
+List* sortList(List* l);
+
+Node* newNodeEmpty();
+Node* newNode(int v);
+Node* findNode(List* l, int v);
+
+int addLast(List* l, int v);
+int addFirst(List* l, int v);
+int removeValue(List* l, int v);
+int removeAllValues(List* l, int v);
+
+
+int addInOrder(List* l, int v);
+int containsValue(List* l, int v);
+int findValue(List* l, int v);
+int len(List* l);
+int isEmpty(List* l);
+
+
+
+void printAll(List* l);
+
+int allowRepeat(List* l);
+int denyRepeat(List* l);
+int containsRepeatedNode(List* l);
+
+#endif
