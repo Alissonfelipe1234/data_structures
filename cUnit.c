89ddacbedd7e8637951ef3201d3f70a58717e510
alissonfelipe1234 <alissonfelipe1234@outlook.com>
2019-08-14 20:24:03 -0300

    testing recursive function

--- cUnit.c
+++ cUnit.c
@@ -1,9 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include "linked_list.c"
 
-int main()
+float powf(float x, float y);
+
+int hasDecimal(float x)
 {
-    printf("teste aqu\n");
+    float integral = (int) x;
+    return 0 != (x - integral);
+}
+float power(float x, float n)
+{
+    if(n==1)
+        return x;
+    if(n == 0)
+        return 1;
+    if (n < 0)
+        return 1/(power(x, -n));
+    if (hasDecimal(n)) //insolve for this time
+        return 0;
+
+    return x * power(x, n-1);
+}
+
+int main()
+{   
+    
+    printf("result of x * n = %f\n", power(2, 0.2));
     return 0;
 }
