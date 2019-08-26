8f6b37e52cd8b653c620b40ade809490e80b5688
alissonfelipe1234 <alissonfelipe1234@outlook.com>
2019-08-25 21:42:47 -0300

    creating new header and power function with fractional exponent

--- myMath.c
+++ myMath.c
@@ -4,6 +4,27 @@
 /*
 please include '#include "myMath.c"' in your code
 */
+//heads
+int arithmetic_progression_sum(int first, int last);
+int hasDecimal(float x);
+int hasDecimal(float x);
+void decimal_to_binary(register int decimal);
+void double_to_fraction(double* numerator, double* denominator);
+int sum(int** vector);
+float power(int base, int exponent);
+double power_dbase(double base, int exponent);
+double power_double(double base, double exponent);
+double root(register double argument, register int index);
+double squared_root(register double base);
+double cube_root(register double base);
+
+//!internals use only
+int vector_sum(int * vector, register int size);
+double tryRoot(register double argument, register int index, register double try, register double decimal_place);
+
+//! DEPRECATEDS
+static unsigned long int int_binary(register int decimal);
+
 
 //it return the sum from first to last
 int arithmetic_progression_sum(int first, int last)
@@ -19,15 +40,6 @@ int hasDecimal(float x)
 {
     float integral = (int) x;
     return 0 != (x - integral);
-}
-//it return the power of base raised to exponent
-float power(int base, int exponent)
-{
-    if(exponent == 0)
-        return 1;
-    if (exponent < 0)
-        return 1/(power(base, -exponent));
-    return base * power(base, exponent-1);
 }
 //given a int in base 10 will return a static unsigned long int into base 2
 //max decimal is 524287 or 1111111111111111111 in binary
@@ -58,6 +70,16 @@ void decimal_to_binary(register int decimal)
     decimal_to_binary(decimal/2);
     printf("%i", decimal%2);    
 }
+//it try to transform a double number into a fraction
+void double_to_fraction(double* numerator, double* denominator)
+{
+    if(hasDecimal(*numerator))
+    {
+        *numerator = *numerator*10;
+        *denominator = *denominator*10;
+        return double_to_fraction(numerator, denominator);
+    }
+}
 //!internal use only
 int vector_sum(int * vector, register int size)
 {
@@ -71,22 +93,47 @@ int sum(int** vector)
 {
     return vector_sum((int*)vector, (int)LEN(vector));
 }
-//it return the power of base raised to exponent for double cases, exponent is int
-double dpower(double base, int exponent)
+//it return the power of base raised to exponent
+float power(int base, int exponent)
 {
     if(exponent == 0)
         return 1;
     if (exponent < 0)
-        return 1/(dpower(base, -exponent));
+        return 1/(power(base, -exponent));
+    return base * power(base, exponent-1);
+}
+//it return the power of double base raised to int exponent
+double power_dbase(double base, int exponent)
+{
+    if(exponent == 0)
+        return 1;
+    if (exponent < 0)
+        return 1/(power_dbase(base, -exponent));
 
-    return base * dpower(base, exponent-1);
+    return base * power_dbase(base, exponent-1);
+}
+//it return the power of double base raised to double exponent
+double power_double(double base, double exponent)
+{
+    if(hasDecimal(exponent))
+    {
+        double numerator = exponent;
+        double denominator = 1;
+        double_to_fraction(&numerator, &denominator);
+        return(root(power_double(base, numerator), (int)denominator));
+    }
+    if(exponent == 0)
+        return 1;
+    if (exponent < 0)
+        return 1/(power_double(base, -exponent));
+
+    return base * power_double(base, exponent-1);
 }
 //!internal use only
 double tryRoot(register double argument, register int index, register double try, register double decimal_place)
 {
-    printf("%lf\n", decimal_place);
-    double tried = dpower(try, index);
-    if(fabs(tried - argument) == 0 || decimal_place <= 0.00000000001)
+    double tried = power_dbase(try, index);
+    if(tried - argument == 0 || decimal_place <= 0.00000000000001)
         return try;
     if(tried > argument)
         return  tryRoot(argument, index, try-decimal_place, decimal_place/10);
