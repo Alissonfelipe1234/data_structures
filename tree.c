e779abc1800ebf8daef625f7c5a32c4f7cb2e7ab
alissonfelipe1234 <alissonfelipe1234@outlook.com>
2019-09-05 21:44:29 -0300

    new create, insert and print's functions

--- tree.c
+++ tree.c
@@ -44,7 +44,7 @@ int containsValue(Tree* l, int v);
 int len(Tree* l);
 int findLevel(Tree* l, int v);
 int isEmpty(Tree* l);
-void printAll(Tree* l);
+void printTree(Tree* l);
 
 //===================================
 //                                   
@@ -56,8 +56,31 @@ void printAll(Tree* l);
 //                                   
 //===================================
 Tree* newTree(int value){
-    return (Tree*) calloc(1, sizeof(Tree));
+    Tree* newest =  (Tree*) calloc(1, sizeof(Tree));
+    newest->value = value;
+    return newest;
 }
-void insertLeave(int value, Tree* root){
-    
+
+void insertLeave(register int value, register Tree* root){
+    if(root==NULL) root = newTree(value);
+    else if(root->value < value) insertLeave(value, root->right);
+    else insertLeave(value, root->left);
+}
+void printTree(Tree* root) {
+    if(root==NULL)
+        return;
+    printTree(root->left);
+    printf(" %i", root->value);
+    printTree(root->right);
+}
+int main()
+{
+    Tree* teste = newTree(5);
+    insertLeave(1, teste);
+    insertLeave(2, teste);
+    insertLeave(3, teste);
+    insertLeave(4, teste);
+    insertLeave(6, teste);
+    printTree(teste);
+    return 0;
 }
