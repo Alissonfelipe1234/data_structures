657f8818884eba6932c64cde7d4e6fc226e10536
alissonfelipe1234 <alissonfelipe1234@outlook.com>
2019-11-13 22:41:22 -0200

    init some exercises

--- shorter.c
+++ shorter.c
@@ -1,16 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
-/** 
-Neste exercício você irá implementar um algoritmo de ordenação misto. Este algoritmo terá como base o QuickSort. A medida que a recursão do QuickSort vai acontecendo, 
-as sub-listas que devem ser ordenadas vão diminuindo de tamanho, ate que chegamos no caso base. 
-Originalmente, este caso base são listas de tamanho um. 
-Neste laboratório nos vamos modificar o tamanho das listas do caso base para um valor threshold que será fornecido como entrada do problema. 
-A ideia e utilizar o InsertionSort para ordenar as sub-listas quando o tamanho desta seja menor do que este threshold. 
-Assim, estaremos utilizando sempre o melhor método de ordenação possível, aproveitando o melhor de cada um.
 
-*/
-
-void quick_sort(char *a, int left, int right) {
+void quick_sort(char *a, int left, int right, int limit) {
     int i, j, x, y;
      
     i = left;
@@ -32,13 +23,35 @@ void quick_sort(char *a, int left, int right) {
             j--;
         }
     }
-     
-    if(j > left) {
-        quick_sort(a, left, j);
+    if(limit >= right - left){
+    
+   		    if(j > left) {
+            quick_sort(a, left, j, limit);
+   				  }
+        if(i < right) {
+            quick_sort(a, i, right, limit);
+        }
     }
-    if(i < right) {
-        quick_sort(a, i, right);
+    else
+    {
+        insertionSort(a, right-left, left);
     }
+}
+
+void insertionSort(char * a, int len, int init)  
+{  
+    int i, key, j;  
+    for (i = init; i < len; i++) 
+    {  
+        key = a[i];  
+        j = i - 1;
+        while (j >= 0 && a[j] > key) 
+        {  
+            a[j + 1] = a[j];  
+            j = j - 1;  
+        }  
+        a[j + 1] = key;  
+    }  
 }
 int main(){
     
