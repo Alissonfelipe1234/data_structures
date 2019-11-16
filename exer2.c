4dd7f1f90d4f3fc8dfa03e6e72d04c82a01707d3
alissonfelipe1234 <alissonfelipe1234@outlook.com>
2019-11-15 23:58:58 -0300

    create main"

--- exer2.c
+++ exer2.c
@@ -1,16 +1,19 @@
-#include<stdlib.h>
-#include<stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-typedef struct _carro{
-    char placa[7]
-    char modelo[15];
+typedef struct _carro
+{
+    char placa[7] char modelo[15];
     unsigned int ano;
     char proprietario[15];
     unsigned int preco;
-}Carro;
+} Carro;
 
 // PLACA MODELO ANO PROPRIETARIO PRECO
 // ABC1234 Palio 2008 Luciano 18000
-int main (){
-    
+int main()
+{
+    int tamanho, carro;
+    scanf("%d", &tamanho);
+    scanf("%d", &carro);
 }
