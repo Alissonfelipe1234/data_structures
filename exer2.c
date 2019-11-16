63689956fb916d1e573e4359405a4918a0dbc253
alissonfelipe1234 <alissonfelipe1234@outlook.com>
2019-11-15 23:51:33 -0300

    init exer2

---
+++ exer2.c
@@ -0,0 +1,14 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+typedef struct _carro{
+    char modelo[16];
+    int ano;
+    char nome[16];
+    int preco;
+    char placa[8];
+}Carro;
+
+int main (){
+    
+}
